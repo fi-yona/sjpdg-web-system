74645c81b5d176c967d591d65d2bb0e7eb84c067
fi-yona <fionaavila15@gmail.com>
2023-03-06 18:13:38 +0700

    Change language

--- function_.php
+++ function_.php
@@ -14,8 +14,8 @@
         <center>
         <div class="footer">
             Copyright UD Sinar Jaya Padang (IT Departement) - 2023
-        </center>
-        </div>  
+        </div>
+        </center>  
         <?php
     }
 
